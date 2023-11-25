73ced34f023d18b447726556ac5d135989b87d94
timotiusalbert <timotiusalbert46@gmail.com>
2023-11-26 06:34:57 +0700

    Update font family and add Step2 component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Step1 from './components/post-a-job/Step1';
+import Step2 from './components/post-a-job/Step2';
 
 
 const App: React.FC = () => {
@@ -11,6 +12,7 @@ const App: React.FC = () => {
       <div className="flex flex-col min-h-screen">
         <Routes>
           <Route path="/post-a-job-step-1" element={<Step1 />} />
+          <Route path="/post-a-job-step-2" element={<Step2 />} />
         </Routes>
         <Footer />
       </div>
